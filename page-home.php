397e87780aadad4c8d3f453cbe460fa847edfaae
Ronny Perez <ronnypdev@gmail.com>
2019-04-17 13:15:14 -0400

    Implemented Carousel Section

--- page-home.php
+++ page-home.php
@@ -21,33 +21,8 @@ get_header();
     <div class="entire-content">
 
         <!-- Slider Section -->
-        <section class="slider" id="slider">
-            <ul class="slider-carousel" id="slider-carousel">
-                <li class="img1">
-                    <h1>Welcome to<span class="primary"> Corporate<span></h1>
-                    <p>It is a long established fact that a reader will be distracted by the readable <br> content of a page when looking at its layout</p>
-                    <a href="#" class="primary-bg cta1">Get Started</a>
-                    <a href="#" class="secondary-sky-blue-bg cta2">Contact Us</a>
-                </li>
-                <li class="img2">
-                    <h1>We are <span class="primary"> Developers<span></h1>
-                    <p>It is a long established fact that a reader will be distracted by the readable <br> content of a page when looking at its layout</p>
-                    <a href="#" class="primary-bg cta1">See Our Works</a>
-                    <a href="#" class="secondary-sky-blue-bg cta2">Our Members</a>
-                </li>
-                <li class="img3">
-                    <h1>We make <span class="primary"> Awesome Themes<span></h1>
-                    <p>It is a long established fact that a reader will be distracted by the readable <br> content of a page when looking at its layout</p>
-                    <a href="#" class="primary-bg cta1">Check Our Themes</a>
-                    <a href="#" class="secondary-sky-blue-bg cta2">Contact Us</a>
-                </li>
-            </ul>
-            <ul class="sliderpager">
-                <li><a href="#"><i class="fa fa-circle"></i></a></li>
-                <li><a href="#"><i class="fa fa-circle"></i></a></li>
-                <li><a href="#"><i class="fa fa-circle"></i></a></li>
-            </ul>
-        </section><!-- End Slider -->
+            <?php get_template_part('template-parts/content', 'carousel'); ?>
+        <!-- # Slider -->
 
         <!-- Services Section -->
         <section class="services section-padding" id="services">
---
+++ template-parts/content-carousel.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Template part for displaying posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package parallax
+ */
+
+?>
+
+<!-- Slider Section -->
+<section class="slider" id="slider">
+    <ul class="slider-carousel" id="slider-carousel">
+        <?php
+        $slides = array(
+            'post_type' => 'slide_content',
+            'orderby' => 'post_id',
+            'order' => 'DESC'
+        );
+
+        $carousel = new WP_Query($slides);
+        ?>
+        <?php while($carousel->have_posts()) : $carousel->the_post(); ?>
+            <?php
+            $btn_cta1 = get_field('btn_cta1');
+            $btn_cta2 = get_field('btn_cta2');
+            $thumbnail_url = wp_get_attachment_url(get_post_thumbnail_id( $post->ID ));
+            ?>
+            <li style="background: url('<?php echo $thumbnail_url; ?>') no-repeat; background-size: 100% 100%;">
+                <h1><?php the_title(); ?></h1>
+                <?php the_content(); ?>
+                <a href="#" class="primary-bg cta1"><?php echo $btn_cta1; ?></a>
+                <a href="#" class="secondary-sky-blue-bg cta2"><?php echo $btn_cta2; ?></a>
+            </li>
+        <?php endwhile; ?>
+    </ul>
+    <ul class="sliderpager">
+        <li><a href="#"><i class="fa fa-circle"></i></a></li>
+        <li><a href="#"><i class="fa fa-circle"></i></a></li>
+        <li><a href="#"><i class="fa fa-circle"></i></a></li>
+    </ul>
+</section><!-- # Slider -->
